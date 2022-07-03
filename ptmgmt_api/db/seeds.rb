1e402a9fe5c8115342c5f1dc7a2d7f685901ec9d
Lunarang <ecmettille@gmail.com>
2022-07-02 18:57:48 -0700

    created seeds

--- ptmgmt_api/db/seeds.rb
+++ ptmgmt_api/db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Attorneys
+attorneys = []
+5.times do
+    attorney = Attorney.create(
+        name: '#{Faker::Company.name} Lawfirm', 
+        address: '#{Faker::Address.street_address}', 
+        city: '#{Faker::Address.city}', 
+        state: '#{Faker::Address.state_abbr}', 
+        zip: Faker::Address.zip, 
+        phone: '#{Faker::PhoneNumber.phone_number}', 
+        fax: '#{Faker::PhoneNumber.phone_number}'
+    )
+    attorneys << attorney
+end
+
+# Create Patients
+patients = []
+gender = ["M", "F"]
+20.times do
+    patient = Patient.create(
+        attorney: attorneys.sample(1),
+        name: '#{Faker::Name.name}',
+        dob: Faker::Date.birthday(min_age: 18, max_age: 80),
+        sex: gender.sample(1),
+        dol: Faker::Date.between(from: '2021-01-01', to: Date.today),
+        initial: Faker::Date.between(from: '2021-01-01', to: Date.today),
+        email: '#{Faker::Internet.email}',
+    )
+    patients << patient
+end
