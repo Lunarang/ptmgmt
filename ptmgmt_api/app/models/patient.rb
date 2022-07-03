d353bfda64806d35496111d4bbb87e6c2b034d37
Lunarang <ecmettille@gmail.com>
2022-07-02 18:57:17 -0700

    added association validations

--- ptmgmt_api/app/models/patient.rb
+++ ptmgmt_api/app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ApplicationRecord
     belongs_to :attorney
     has_many :treatments, :imagings, :referrals
     validates_presence_of :attorney, :name, :dob, :sex, :dol, :initial, :email
+    validates_associated :treatments, :imagings, :referrals
 end
