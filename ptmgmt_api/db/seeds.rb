1fe7bbceac47c1705b6b980d2bf716036b2a51e5
Lunarang <ecmettille@gmail.com>
2022-07-12 21:24:22 -0700

    fixed seed generation

--- ptmgmt_api/db/seeds.rb
+++ ptmgmt_api/db/seeds.rb
@@ -1,30 +1,26 @@
 # Create Attorneys
-attorneys = []
 5.times do
     attorney = Attorney.create(
-        name: '#{Faker::Company.name} Lawfirm', 
-        address: '#{Faker::Address.street_address}', 
-        city: '#{Faker::Address.city}', 
-        state: '#{Faker::Address.state_abbr}', 
+        name: "#{Faker::Company.name} Lawfirm", 
+        address: "#{Faker::Address.street_address}", 
+        city: "#{Faker::Address.city}", 
+        state: "#{Faker::Address.state_abbr}", 
         zip: Faker::Address.zip, 
-        phone: '#{Faker::PhoneNumber.phone_number}', 
-        fax: '#{Faker::PhoneNumber.phone_number}'
+        phone: "#{Faker::PhoneNumber.phone_number}", 
+        fax: "#{Faker::PhoneNumber.phone_number}"
     )
-    attorneys << attorney
 end
 
 # Create Patients
-patients = []
-gender = ["M", "F"]
 20.times do
     patient = Patient.create(
-        attorney: attorneys.sample(1),
-        name: '#{Faker::Name.name}',
+        attorney: Attorney.all.sample,
+        first_name: "#{Faker::Name.first_name }",
+        last_name: "#{Faker::Name.last_name}",
         dob: Faker::Date.birthday(min_age: 18, max_age: 80),
-        sex: gender.sample(1),
-        dol: Faker::Date.between(from: '2021-01-01', to: Date.today),
-        initial: Faker::Date.between(from: '2021-01-01', to: Date.today),
-        email: '#{Faker::Internet.email}',
+        sex: Faker::Gender.short_binary_type,
+        dol: Faker::Date.between(from: "2021-01-01", to: Date.today),
+        initial: Faker::Date.between(from: "2021-01-01", to: Date.today),
+        email: "#{Faker::Internet.email}"
     )
-    patients << patient
 end
