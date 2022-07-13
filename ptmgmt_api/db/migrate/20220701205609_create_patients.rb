fff836b89691e5f7c197b5c5e80fb57609f3c997
Lunarang <ecmettille@gmail.com>
2022-07-12 21:24:03 -0700

    created separations of patient first and last name for search purposes

--- ptmgmt_api/db/migrate/20220701205609_create_patients.rb
+++ ptmgmt_api/db/migrate/20220701205609_create_patients.rb
@@ -1,7 +1,8 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
     create_table :patients do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.date :dob
       t.string :sex
       t.date :dol
