274f4482acf0b5516bd5ec343278fc87431165c2
Lunarang <ecmettille@gmail.com>
2022-07-15 22:34:09 -0700

    restructured directories again... decided to use RTK's configureStore because it is a more commonplace  modern practice

--- ptmgmt_client/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import patientReducer from '../features/patients/patientSlice'
-
-// by default uses thunk middleware and dev tools extension compatibility
-export const store = configureStore({
-  reducer: {
-    patients: patientReducer
-  },
-});
