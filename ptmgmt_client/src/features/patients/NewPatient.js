3f4c717c9cf952d5df2b40ed99b753b81316544b
Lunarang <ecmettille@gmail.com>
2022-07-15 22:38:45 -0700

    restructured directories again...

--- ptmgmt_client/src/features/patients/NewPatient.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class NewPatient extends Component {
-  render() {
-    return <div>New Patient Form</div>
-  }
-};
-
-export default NewPatient;
--- ptmgmt_client/src/features/patients/Patient.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Patient extends Component {
-  render() {
-    return <div>Patient Profile</div>
-  }
-};
-
-export default Patient;
