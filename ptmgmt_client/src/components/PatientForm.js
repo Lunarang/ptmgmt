189eadfa89f5199b1afd221a82317deee62dc16d
Lunarang <ecmettille@gmail.com>
2022-07-17 16:06:49 -0700

    basic form set up to make sure routes are working properly

--- ptmgmt_client/src/components/PatientForm.js
+++ ptmgmt_client/src/components/PatientForm.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 
+// controlled form - derives input values from state
 class PatientForm extends Component {
+  state = {
+    first_name: '',
+  }
+
+  handleSubmit = (event) => {
+    // event.preventDefault()
+    // this.sendFormData(this.state)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      first_name: event.target.value,
+    })
+  }
+  
   render() {
-    return <div>New Patient Form</div>
+    return (
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input 
+            type="text" 
+            name="first_name" 
+            onChange={event => this.handleChange(event)} 
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    )
   }
 };
 
-export default PatientForm;
+export default PatientForm
