28b49c274ef3d24338484d27e9267fbd4003b091
Lunarang <ecmettille@gmail.com>
2022-07-17 22:06:20 -0700

    removed unnecessary link

--- ptmgmt_client/src/components/Home.js
+++ ptmgmt_client/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import { PatientIndex } from './PatientIndex'
 
 class Home extends Component {
   render() {
-    return <div>Hi Friends! <Link to="/patients/1">View patient profile</Link><PatientIndex /></div>
+    return (
+      <div>
+        Hi Friends!
+        <PatientIndex />
+      </div>
+    )
   }
 };
 
