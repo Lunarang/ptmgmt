c1f0bcd3886983d7cddf36d78b9c09a5488521ac
Lunarang <ecmettille@gmail.com>
2022-07-17 22:09:40 -0700

    updated from class to functional component as it does not need to us state whatsoever

--- ptmgmt_client/src/components/PatientForm.js
+++ ptmgmt_client/src/components/PatientForm.js
@@ -1,36 +1,19 @@
-import React, { Component } from 'react'
-
 // controlled form - derives input values from state
-class PatientForm extends Component {
-  state = {
-    first_name: '',
-  }
-
-  handleSubmit = (event) => {
-    // event.preventDefault()
-    // this.sendFormData(this.state)
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      first_name: event.target.value,
-    })
-  }
-  
-  render() {
-    return (
-      <div>
-        <form onSubmit={event => this.handleSubmit(event)}>
-          <input 
-            type="text" 
-            name="first_name" 
-            onChange={event => this.handleChange(event)} 
-          />
-          <input type="submit" />
-        </form>
-      </div>
-    )
-  }
+// info passed in as props from PatientAdd or PatientEdit
+function PatientForm(props) {
+  return (
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <input 
+          type="text" 
+          name="first_name" 
+          onChange={event => props.handleChange(event)} 
+          value={props.formData.first_name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 };
 
 export default PatientForm
