2c525e0abdba373a3950e53cfdf09030244e90bb
Lunarang <ecmettille@gmail.com>
2022-07-19 13:16:50 -0700

    fleshed out displayed patient info and fixed patient selector hook to return correct patient, as patient objects' id attribute does not match their index

--- ptmgmt_client/src/components/Patient.js
+++ ptmgmt_client/src/components/Patient.js
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function Patient() {
   const id = useParams().id
+  const index = id - 1
+  const patient = useSelector(state => state.patients.data[index])
 
   return (
     <div>
-      Patient Profile
+      <h2>{patient.first_name} {patient.last_name}</h2>
+      Date of Birth: {patient.dob}
+      Sex: {patient.sex}
+      Email: {patient.email}
+      Referred By: {patient.referred_by}
+      
+      Date of Loss: {patient.dol}
+      Initial Visit: {patient.initial}
+      Attorney: {patient.attorney}
+      Case Manager: {patient.case_manager}
+      Notes: {patient.notes}
+
       <Link to={`/patients/${id}/edit`} className="button muted-button">
         Edit
       </Link>
