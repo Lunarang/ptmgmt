0dd9a5aa346a3ff7b96332034bd47282ccfec4ba
Lunarang <ecmettille@gmail.com>
2022-07-20 22:04:59 -0700

    fixed navigation url to redirect to patient's profile by id

--- ptmgmt_client/src/components/PatientEdit.js
+++ ptmgmt_client/src/components/PatientEdit.js
@@ -1,34 +1,38 @@
 import React, { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { editPatient } from '../reducers/patientReducer'
+import { editPatient, selectPatientById } from '../reducers/patientReducer'
 import PatientForm from './PatientForm'
 
 function PatientEdit() {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const patientId = useParams().id
-    const index = patientId - 1
-    const patientData = useSelector(state => state.patients.data[index])
+    const id = parseInt(useParams().id)
+
+    // find patient with the matching id
+    const patientData = useSelector(state => selectPatientById(state, id))
+    // const index = patientId - 1
+    // const patientData = useSelector(state => state.patients.data[index])
     
     const [formData, setFormData] = useState({
-        first_name: '',
-        last_name: '',
-        dob: '',
-        sex: '',
-        email: '',
-        referred_by: '',
-        dol: '',
-        initial: '',
-        attorney_id: 0,
-        case_manager: '',
-        notes: ''
+        first_name: patientData.first_name,
+        last_name: patientData.last_name,
+        dob: patientData.dob,
+        sex: patientData.sex,
+        email: patientData.email,
+        referred_by: patientData.referred_by || '',
+        dol: patientData.dol,
+        initial: patientData.initial,
+        attorney_id: patientData.attorney_id,
+        case_manager: patientData.case_manager || '',
+        notes: patientData.notes || ''
       })
 
     function handleSubmit(event) {
         event.preventDefault()
-        dispatch(editPatient(formData, patientId))
-        navigate(`/patients/${patientId}`)
+        dispatch(editPatient(formData, id))
+        navigate(`/patients/${id}`, {replace: true})
+        // redirect to profile, but do not allow user to return to edit page with back button
     }
 
     function handleChange(event) {
@@ -47,7 +51,7 @@ function PatientEdit() {
         <div>
         <h1>Edit Patient</h1>
         <PatientForm 
-            formData={patientData} 
+            formData={formData} 
             handleChange={handleChange} 
             handleSubmit={handleSubmit}
         />
