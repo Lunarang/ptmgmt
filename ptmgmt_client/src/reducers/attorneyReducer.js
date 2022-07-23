ed0816062a05cb19aea71b1620cac7bb3c610f1c
Lunarang <ecmettille@gmail.com>
2022-07-22 18:30:03 -0700

    gave reducer a more complex initial state to reflect status of fetch requests

--- ptmgmt_client/src/reducers/attorneyReducer.js
+++ ptmgmt_client/src/reducers/attorneyReducer.js
@@ -1,9 +1,34 @@
-const attorneyReducer = (state = [], action) => {
+const initialState = {
+  data: [],
+  status: 'idle',
+  error: null
+};
+
+const attorneyReducer = (state = initialState, action) => {
     switch(action.type){
-      case 'ADD_ATTORNEY':
-        return
-      case 'FETCH_ATTORNEYS_FULFILLED':
-        return action.payload
+      case 'ADD_ATTORNEY': {
+        return {}
+      }
+      case 'FETCH_ATTORNEYS_PENDING': {
+        return {
+          ...state,
+          status: 'pending'
+        }
+      }
+      case 'FETCH_ATTORNEYS_FULFILLED': {
+        return {
+          ...state,
+          status: 'succeeded',
+          data: action.payload
+        }
+      }
+      case 'FETCH_ATTORNEYS_REJECTED': {
+        return {
+          ...state,
+          status: 'failed',
+          error: action.payload.error.message
+        }
+      }
       default:
         return state
     }
@@ -11,9 +36,13 @@ const attorneyReducer = (state = [], action) => {
   
   export default attorneyReducer
   
-  export const selectAllAttorneys = state => state.attorneys
+  export const selectAllAttorneys = state => state.attorneys.data
   
+  export const selectAttorneyById = (state, id) => 
+  state.attorneys.data.find(attorney => attorney.id === id)
+
   export const fetchAttorneys =  () => async (dispatch) => {
+    dispatch({ type: 'FETCH_ATTORNEYS_PENDING' })
     const response = await fetch('http://127.0.0.1:3001/attorneys')
     const attorneys = await response.json()
     dispatch({ type: 'FETCH_ATTORNEYS_FULFILLED', payload: attorneys })
