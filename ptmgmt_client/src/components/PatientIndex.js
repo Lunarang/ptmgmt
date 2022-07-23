989ec15950f383096a0898665198fd8564fcf419
Lunarang <ecmettille@gmail.com>
2022-07-22 18:29:12 -0700

    added a conditional statement to prevent content from being loaded until all fetch requests are complete

--- ptmgmt_client/src/components/PatientIndex.js
+++ ptmgmt_client/src/components/PatientIndex.js
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux'
 import { selectAllPatients } from '../reducers/patientReducer'
 import { Link } from 'react-router-dom'
+import { selectAttorneyById } from '../reducers/attorneyReducer';
 
 const PatientExcerpt = ({ patient }) => {
+    const attorney = useSelector(state => selectAttorneyById(state, patient.attorney_id))
     return (
       <div className="patient-excerpt" key={patient.id}>
         <h3>{patient.first_name} {patient.last_name}</h3>
+        {patient.sex} {patient.dob} {patient.initial} {attorney.name}
         <Link to={`/patients/${patient.id}`} className="button muted-button">
           View Patient
         </Link>
@@ -15,20 +18,16 @@ const PatientExcerpt = ({ patient }) => {
 
 export const PatientIndex = () => {
   const patients = useSelector(selectAllPatients)
+  const patientStatus = useSelector(state => state.patients.status)
+  const attorneyStatus = useSelector(state => state.attorneys.status)
 
-  const content = patients.map(patient => (
-    <PatientExcerpt key={patient.id} patient={patient} />
-  ))
+  let content
 
-  // if (patientStatus === 'loading') {
-  //   content = <p>Loading...</p>
-  // } else if (patientStatus === 'succeeded') {
-  //   content = patients.map(patient => (
-  //     <PatientExcerpt key={patient.id} patient={patient} />
-  //   ))
-  // } else if (patientStatus === 'failed') {
-  //   content = <div>{error}</div>
-  // }
+  if (patientStatus === 'succeeded' && attorneyStatus === 'succeeded') {
+    content = patients.map(patient => (
+      <PatientExcerpt key={patient.id} patient={patient} />
+    ))
+  }
 
   return (
     <section className="patient-index">
