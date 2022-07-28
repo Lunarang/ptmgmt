4c3a70a81ffce3d3aeacf971da01a6c5185f9d9e
Lunarang <ecmettille@gmail.com>
2022-07-28 00:30:12 -0700

    added views for attorneys

--- ptmgmt_client/src/routes/Views.jsx
+++ ptmgmt_client/src/routes/Views.jsx
@@ -4,6 +4,8 @@ import PatientAdd from '../components/PatientAdd'
 import PatientEdit from '../components/PatientEdit'
 import Patient from '../components/Patient'
 import NotFound from './NotFound'
+import Attorney from '../components/Attorney'
+// import AttorneyEdit from '../components/AttorneyEdit'
 
 const Views = () => {
     return (
@@ -13,6 +15,10 @@ const Views = () => {
                 <Route path="new" element={<PatientAdd />} />
                 <Route path=":id" element={<Patient />} />
                 <Route path=":id/edit" element={<PatientEdit />} />
+            </Route>
+            <Route path="/attorneys">
+                <Route path=":id" element={<Attorney />} />
+                {/* <Route path=":id/edit" element={<AttorneyEdit />} /> */}
             </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
