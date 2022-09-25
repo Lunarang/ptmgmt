c53cc581fd0552cad2829f46951ae354bfc10279
Lunarang <ecmettille@gmail.com>
2022-09-24 22:46:05 -0700

    tell Devise in which controller it can find OmniAuth callbacks

--- ptmgmt_api/config/routes.rb
+++ ptmgmt_api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :attorneys
   resources :referrals
   resources :imagings
