534ad21d43a172611612ba8f2a6f604dd68a7810
Lunarang <ecmettille@gmail.com>
2022-09-25 22:58:30 -0700

    added export for selecting all referrals from the store, and added get/post/patch/delete fetch requests

--- ptmgmt_client/src/reducers/referralReducer.js
+++ ptmgmt_client/src/reducers/referralReducer.js
@@ -33,3 +33,47 @@ const referralReducer = (state = [], action) => {
 };
   
 export default referralReducer
+
+// Select all referrals
+export const selectAllReferrals = state => state.referrals
+
+// GET
+export const fetchReferrals =  () => async (dispatch) => {
+const response = await fetch('http://127.0.0.1:3001/referrals')
+const referrals = await response.json()
+dispatch({ type: 'FETCH_REFERRALS_FULFILLED', payload: referrals })
+}
+
+// POST
+export const addReferral = data => async (dispatch) => {
+const response = await fetch('http://127.0.0.1:3001/referrals', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+    referral: data
+    })
+})
+const newReferral = await response.json()
+dispatch({ type: 'ADD_REFERRAL', payload: newReferral })
+}
+
+// PATCH
+export const editReferral = (data, id) => async (dispatch) => {
+const response = await fetch(`http://127.0.0.1:3001/referrals/${id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+    referral: data
+    })
+})
+const updatedReferral = await response.json()
+dispatch({ type: 'EDIT_REFERRAL', payload: updatedReferral })
+}
+
+// DELETE
+export const deleteReferral = (id) => async (dispatch) => {
+const response = await fetch(`http://127.0.0.1:3001/referrals/${id}`, {method: 'DELETE'})
+const confirmation = await response.json()
+console.log(confirmation)
+dispatch({ type: 'DELETE_REFERRAL', payload: id })
+}
