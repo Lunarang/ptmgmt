3316c9f2c255c0c4f459fba191e4a63c336cdc14
Lunarang <ecmettille@gmail.com>
2022-09-25 23:17:41 -0700

    updated schema to include 'method' column for Imagings table

--- ptmgmt_api/db/schema.rb
+++ ptmgmt_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_25_052235) do
+ActiveRecord::Schema.define(version: 2022_09_26_061351) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_09_25_052235) do
     t.integer "patient_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "method"
     t.index ["patient_id"], name: "index_imagings_on_patient_id"
   end
 
