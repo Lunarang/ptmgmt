79fd557e3ddef37d14781e89ce87c3b410356107
Lunarang <ecmettille@gmail.com>
2022-09-25 22:41:49 -0700

    imported Referral reducer and added to Redux store

--- ptmgmt_client/src/store.js
+++ ptmgmt_client/src/store.js
@@ -3,12 +3,14 @@ import patientReducer from './reducers/patientReducer'
 import attorneyReducer from './reducers/attorneyReducer'
 import treatmentReducer from './reducers/treatmentReducer'
 import imagingReducer from './reducers/imagingReducer'
+import referralReducer from './reducers/referralReducer'
 
 const reducer = {
   patients: patientReducer,
   attorneys: attorneyReducer,
   treatments: treatmentReducer,
   imagings: imagingReducer,
+  referrals: referralReducer,
 };
 
 const store = configureStore({
