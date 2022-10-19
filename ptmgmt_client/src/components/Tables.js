58e88f22124d796ccc9df3eabcfaab9c725024a1
Lunarang <ecmettille@gmail.com>
2022-10-18 22:45:48 -0700

    playing with self contained custom editable table function

---
+++ ptmgmt_client/src/components/Tables.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addItem, editItem, deleteItem, selectAllItems } from '../reducers/itemReducer'
+
+function Tables(props) {
+
+}
+
+export default Tables
